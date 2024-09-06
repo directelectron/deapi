a582d67727710a2fd328f8567d63607f5a554519
Carter Francis <csfrancis@wisc.edu>
2024-09-06 14:19:40 -0500

    Testing: Add commandline for running pydeserver

--- deapi/simulated_server/initialize_server.py
+++ deapi/simulated_server/initialize_server.py
@@ -1,23 +1,33 @@
 import sys
 
-from fake_server import FakeServer
+from deapi.simulated_server.fake_server import FakeServer
 import socket
 import struct
 from deapi.buffer_protocols import pb
-import logging
 import sys
-import time
+import argparse
 
 
 # Defining main function
 def main(port=13240):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, help="Port to listen on")
+    args = parser.parse_args()
+    if args.port:
+        port = args.port
 
     HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
     PORT = port  # Port to listen on (non-privileged ports are > 1023)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind((HOST, PORT))
         server_socket.listen()
-        sys.stderr.write("started")
+        sys.stderr.write("Server started.... \n\n")
+        sys.stderr.flush()
+        sys.stderr.write(
+            "Waiting for a Connection to: \n"
+            f"    Host: {HOST}\n"
+            f"    Port: {PORT} \n"
+        )
         sys.stderr.flush()
         while True:
             conn, addr = server_socket.accept()  # What waits for a connection
