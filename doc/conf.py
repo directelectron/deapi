6ead8e07bdb3a1f07dd38be61d9c829cae75a40d
Carter Francis <csfrancis@wisc.edu>
2025-09-23 15:51:59 -0500

    Update conf.py file to correctly find the static style sheets.

--- doc/conf.py
+++ doc/conf.py
@@ -1,19 +1,13 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# Configuration file for the Sphinx documentation builder
+import os
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = "deapi"
 copyright = "2024, DE Developers"
 author = "DE Developers"
 release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
@@ -21,36 +15,29 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx_gallery.gen_gallery",
 ]
+
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
+# -- HTML output options -----------------------------------------------------
 html_theme = "pydata_sphinx_theme"
 html_static_path = ["_static"]
 html_logo = "_static/de_api_icon.svg"
-
 master_doc = "index"
 
+# -- Base URL for GitHub Pages / PR previews ---------------------------------
+pr_number = os.environ.get("GITHUB_PR_NUMBER")  # set in workflow
+if pr_number:
+    html_baseurl = (
+        f"https://previewde.github.io/deapi-preview/pr-preview/pr-{pr_number}/"
+    )
+else:
+    html_baseurl = "https://directelectron.github.io/deapi/"  # main branch
 
-# sphinx.ext.autodoc
-# ------------------
-# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html
-autosummary_ignore_module_all = False
-autosummary_imported_members = True
-autodoc_typehints_format = "short"
-autodoc_default_options = {
-    "show-inheritance": True,
-}
+# Use absolute URLs so CSS and static files work in PR previews
+html_use_relative_urls = False
 
-autosummary_generate = True
-
-# sphinx_gallery
-# --------------
-# https://sphinx-gallery.github.io/stable/configuration.html
-
+# -- Sphinx Gallery ----------------------------------------------------------
 sphinx_gallery_conf = {
     "examples_dirs": "../examples",
     "gallery_dirs": "examples",
@@ -58,7 +45,42 @@ sphinx_gallery_conf = {
     "ignore_pattern": "_sgskip.py",
     "backreferences_dir": "api",
     "doc_module": ("deapi",),
-    "reference_url": {
-        "deapi": None,
-    },
+    "reference_url": {"deapi": None},
 }
+
+# -- Autodoc / Autosummary ---------------------------------------------------
+autosummary_generate = True
+autosummary_imported_members = True
+autosummary_ignore_module_all = False
+
+autodoc_typehints_format = "short"
+autodoc_default_options = {
+    "show-inheritance": True,
+}
+
+# -- Optional: Include custom CSS -------------------------------------------
+# If you have a custom CSS file in _static, e.g., _static/custom.css
+html_css_files = [
+    "custom.css",
+]
+
+# -- Optional: Sphinx-Gallery memory logging ---------------------------------
+sphinx_gallery_conf["show_memory"] = True
+
+# -- nbsphinx (if using notebooks) -------------------------------------------
+nbsphinx_execute = "never"
+nbsphinx_kernel_name = "python3"
+nbsphinx_allow_errors = True
+
+# -- sphinxcontrib-bibtex ---------------------------------------------------
+bibtex_bibfiles = ["bibliography.bib"]
+
+
+# -- Autodoc skip callback ---------------------------------------------------
+def autodoc_skip_member(app, what, name, obj, skip, options):
+    # Never skip members
+    return False
+
+
+def setup(app):
+    app.connect("autodoc-skip-member", autodoc_skip_member)
