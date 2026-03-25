37db4a4f4bdfbd6575cbd516aa19877b9cccff5c
CoolFanyu <pzhang@directelectron.com>
2026-03-24 18:01:40 -0700

    Modify gain acquisition
    Modified the trial function so the logic match with DE-Server. Correct the property name. Add parameter num_acq in take_gain_acquisition so user could save time for specific purpose.

--- deapi/client.py
+++ deapi/client.py
@@ -2352,7 +2352,7 @@ class Client:
 
         if counting:
             self["Image Processing - Mode"] = "Counting"
-            self["Reference - Counting Gain Target (ADU/pix)"] = (
+            self["Reference - Counting Gain Target (e-/pix)"] = (
                 2000
                 if target_electrons_per_pixel is None
                 else target_electrons_per_pixel
@@ -2374,16 +2374,20 @@ class Client:
         while self.acquiring:
             time.sleep(2)
 
+        img, dtype, attr, _ = self.get_result(FrameType.SUMINTERMEDIATE, PixelFormat.UINT16)
+
         if counting:
             exposure_time = self["Reference - Counting Gain Exposure Time (seconds)"]
             total_acquisitions = self["Reference - Counting Gain Acquisitions"]
+            num_el = np.max([attr.eppixpf * frame_rate, attr.eppixps])
+            log.info(f"The number of electrons per pixel per second (eppixps): {num_el:.2f}")
         else:
             exposure_time = self["Reference - Integrating Gain Exposure Time (seconds)"]
             total_acquisitions = self["Reference - Integrating Gain Acquisitions"]
+            num_el = attr.imageMean * frame_rate
+            log.info(f"The number of ADUs per pixel per second : {num_el:.2f}")
 
-        img, dtype, attr, _ = self.get_result(FrameType.SUMTOTAL, PixelFormat.FLOAT32)
         self.SetProperty("Exposure Mode", prevExposureMode)
-
         self.SetProperty("Exposure Time (seconds)", prevExposureTime)
 
         num_el = np.max([attr.eppixpf * frame_rate, attr.eppixps])
@@ -2396,10 +2400,11 @@ class Client:
                 "The trial gain reference image has pixels that are close to saturation. "
                 "Please reduce the beam intensity or exposure time."
             )
+
         # recalculating to check...
-        total_acquisitions = int(
-            np.ceil(target_electrons_per_pixel / (exposure_time * num_el))
-        )
+        # total_acquisitions = int(
+            # np.ceil(target_electrons_per_pixel / (exposure_time * num_el))
+        # )
         if total_acquisitions == 1:
             total_acquisitions = 2
 
@@ -2413,6 +2418,7 @@ class Client:
         target_electrons_per_pixel: float = None,
         timeout: int = 600,
         counting: bool = False,
+        num_acq: int = 0
     ):
         """Take a gain reference.
 
@@ -2439,6 +2445,8 @@ class Client:
             If True, the gain reference will be taken in counting mode, by default False.
             This is useful for cameras that support counting mode and can be used to take gain references
             with a lower noise level.
+        num_acq: int, optional
+            Force the number of acquisiton to this number, otherwise it will be automatically calculated.
         """
         if target_electrons_per_pixel is None and not counting:
             target_electrons_per_pixel = 16000
@@ -2448,6 +2456,9 @@ class Client:
         exposure_time, num_acquisitions, _ = self.take_trial_gain_reference(
             frame_rate, target_electrons_per_pixel, counting
         )
+
+        if (num_acq != 0):
+            num_acquisitions = num_acq
 
         log.info(
             f"Gain reference: {exposure_time:.2f} seconds, "
